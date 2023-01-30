7337aae610eeb2a7ea121ab5c513d5587b84ffd4
Lane Schwartz <dowobeha@gmail.com>
2023-01-30 08:02:53 -0900

    Update

--- aurora.sh
+++ aurora.sh
@@ -56,36 +56,69 @@ L=","      # comma
 M="!"      # exclamation point
 N=" "      # space
 
+#declare -a array=(53 90 201 63 99 135 74 75 111 112 182 183 117 153)
+declare -a array=(71 72 73 74 75 183)
+#
+# see https://en.wikipedia.org/wiki/ANSI_escape_code#8-bit
 
-                # The code \033[_;1m changes the terminal foreground color
-O="\033[92;1m"  # 92 means green
-P="\033[0;1m"   #  0 means reset
+
+function random_color { # Random Element From
+  r=$((RANDOM % ${#array[@]}))
+  printf "%s\n" "${array[$r]}"
+}
+
+ESC="\033"      # ASCII ESC character, encoded as the octal number 33 (equivalent to 27 in decimal and 1B in hexadecimal)
+                #
+                # see https://en.wikipedia.org/wiki/Escape_character#ASCII_escape_character
+
+CSI="${ESC}["   # ANSI Control Sequence Introducer, encoded as the ASCII ESC character followed by an open square bracket
+                #
+                # see https://en.wikipedia.org/wiki/ANSI_escape_code#Fe_Escape_sequences
+                # and https://en.wikipedia.org/wiki/ANSI_escape_code#CSIsection
+
+function foreground_color {
+  printf "${ESC}[38;5;$1;1m"
+}
+
+# The code \033[_;1m changes the terminal foreground color
+#O="${ESC}[38;5;$(random_color);1m"
+
+function O {
+    printf "$(foreground_color $(random_color))"
+}
+
+P="${ESC}[0;1m"   #  0 means reset
 
 
 clear
+
+cat ~/.aurora/next
+
+#mv ~/.aurora/next ~/.aurora/previous.$(date --iso-8601=minutes)
+
 echo -e \
-"$O$N$O$N$O$N$O$N$O$B$O$N$O$C$O$N$O$A$O$N$O$A$O$N$O$A$O$N$O$A$O$C$O$N$O$N$O$A$O$A$O$N$O$N$O$C$O$N$O$N$O$N$O$N$O$N$O$B$O$N$O$N$O$C$O$N$O$N$O$A$O$N$O$N$O$A$O$D$O$A$O$C$O$N$O$A$O$N$O$C$O$N$O$C$O$N$O$C$O$A$O$N$O$N$O$N$O$F$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$B$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$F$O$N
-$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$B$O$N$O$C$O$N$O$A$O$N$O$C$O$A$O$N$O$N$O$A$O$A$O$N$O$N$O$A$O$C$O$N$O$N$O$C$O$N$O$N$O$N$O$N$O$B$O$N$O$N$O$A$O$N$O$N$O$A$O$N$O$C$O$A$O$N$O$C$O$N$O$A$O$N$O$C$O$N$O$A$O$C$O$N$O$N$O$N$O$A$O$N$O$N$O$F$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$C$O$N
-$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$P$F$O$O$O$N$O$O$O$C$O$N$O$N$O$A$O$A$O$N$O$N$O$A$O$C$O$N$O$N$O$A$O$N$O$N$O$O$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$B$O$N$O$A$O$A$O$N$O$A$O$N$O$C$O$N$O$A$O$N$O$A$O$C$O$N$O$C$O$N$O$A$O$N$O$N$O$N$O$F$O$N$O$N$O$B$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$F$O$N$O$N$O$N$O$C$O$F$O$N
-$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$B$O$O$O$N$O$N$O$A$O$A$O$N$O$N$O$A$O$N$O$N$O$O$O$N$O$A$O$N$O$N$O$N$P$E$O$N$O$N$O$N$O$N$O$B$O$N$O$C$O$N$O$A$O$N$O$A$O$N$O$C$O$A$O$N$O$A$P$E$O$A$O$N$O$N$O$C$O$N$O$N$O$N$O$C$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$C$O$A$O$N
-$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$P$E$O$N$O$N$O$N$O$N$P$E$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$B$O$N$O$N$O$A$O$N$O$N$O$C$O$N$O$C$O$N$O$N$O$A$O$N$O$N$O$F$O$N$O$N$O$N$O$N$O$N$O$N$O$B$O$N$O$O$O$N$O$C$O$A$O$N$O$A$O$N$O$C$O$A$O$N$O$F$O$N$O$C$O$N$O$C$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$P$E$O$N$O$N$O$N$O$C$O$F$O$A$O$A$O$N
-$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$P$F$O$B$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$A$O$N$O$A$O$N$O$N$O$A$O$N$O$N$O$C$O$N$O$F$O$C$O$A$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$B$O$N$O$A$O$N$O$A$O$A$O$N$O$A$O$N$O$C$O$N$O$A$O$C$O$N$O$A$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$A$O$N$O$A$O$A$O$N
-$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$O$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$P$F$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$D$O$N$O$B$O$N$O$N$O$A$O$N$O$N$O$C$O$N$O$N$O$F$O$C$O$N$O$F$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$O$O$A$O$N$O$A$O$N$O$C$O$N$O$C$O$A$O$N$O$C$O$N$O$N$O$N$O$N$O$F$O$N$O$N$O$N$O$A$O$C$O$A$O$N$O$A$O$A$O$N
-$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$P$F$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$P$F$O$N$O$N$O$N$O$N$O$B$O$N$P$E$O$A$O$N$O$N$O$A$O$A$O$N$O$C$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$B$O$N$O$N$O$N$O$N$O$A$O$N$O$A$O$N$O$C$O$A$O$N$O$A$O$N$O$C$O$N$O$N$O$N$O$N$O$N$O$N$O$A$O$C$O$A$O$N$O$A$O$N$O$N
-$O$N$O$N$O$N$P$F$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$P$F$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$P$F$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$A$O$A$O$N$O$A$O$F$O$C$O$N$P$E$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$A$O$N$O$A$O$A$O$N$O$C$O$N$O$C$O$N$O$N$O$N$O$N$O$N$O$C$O$A$O$A$O$A$O$N$O$N$O$N$O$N
-$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$P$F$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$P$F$O$N$O$N$O$N$P$F$O$N$P$E$O$N$O$N$O$N$O$N$P$F$O$N$O$N$O$N$P$F$O$N$O$N$O$B$O$N$O$A$O$A$O$A$O$F$O$N$O$N$O$D$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$D$O$N$O$O$O$A$O$N$O$A$O$A$O$A$O$N$O$N$O$F$O$N$O$N$O$A$O$A$O$B$O$N$O$N$O$N$O$N$O$N
-$O$N$O$N$O$N$O$N$O$N$O$N$O$N$P$F$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$P$E$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$P$F$O$N$O$N$O$N$O$N$O$N$O$D$O$C$O$B$O$A$O$M$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$F$O$N$O$A$O$A$O$A$O$A$O$N$O$N$O$C$O$F$O$A$O$A$O$B$O$N$O$N$O$N$O$N$O$N$O$N
-$O$N$O$N$O$N$O$D$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$P$F$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$F$O$F$O$M$O$A$P$E$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$F$O$N$O$A$O$N$O$C$O$B$O$A$O$A$O$D$O$N$O$A$O$A$O$A$O$B$O$N$O$N$O$N$O$N$O$N$O$N$O$N
-$O$N$O$N$O$N$O$N$O$N$O$N$O$N$P$F$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$D$O$N$O$N$O$N$O$N$O$N$O$N$O$C$O$N$O$A$O$A$O$A$O$B$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$F$O$A$O$N$O$C$O$A$O$N$O$A$O$N$O$A$O$N$O$A$O$F$O$A$O$N$O$A$O$A$O$B$O$N$O$N$O$N$O$N$O$N$P$F$O$N$O$N
-$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$P$E$O$N$O$N$O$N$O$N$O$N$O$D$O$N$O$N$O$N$O$O$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$D$O$N$O$N$O$C$O$A$O$A$O$N$O$A$O$B$O$N$O$N$O$N$O$N$O$N$O$C$O$F$O$D$O$F$O$N$O$A$O$A$O$N$O$A$O$A$O$N$O$A$O$N$O$A$O$C$O$B$O$A$O$A$O$N$O$B$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N
-$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$P$F$O$N$O$N$O$N$O$N$O$N$O$N$P$F$O$A$O$A$O$B$O$N$O$F$O$N$O$N$O$N$O$N$O$F$O$F$O$A$O$A$O$N$O$A$O$N$O$A$O$C$O$N$O$O$O$B$O$N$O$B$O$A$O$N$O$A$O$N$O$A$O$B$O$N$O$N$O$D$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N
-$O$N$O$N$O$N$O$N$P$F$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$D$O$D$O$D$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$A$O$A$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$M$O$A$O$M$O$C$O$N$O$B$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$C$O$A$O$N$O$A$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N
-$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$D$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$P$F$O$N$O$N$O$N$O$N$O$N$O$N$P$F$O$N$O$N$O$N$O$N$O$A$O$N$P$F$O$N$O$N$O$N$O$N$O$N$O$N$O$B$O$A$O$A$O$A$O$F$O$C$O$N$O$N$O$N$O$N$O$N$O$N$O$F$O$A$O$A$O$N$O$N$O$N$O$N$O$F$O$N$O$N$O$N$O$N$O$N$O$N$O$F$O$N$O$N$O$N$O$N$O$B
-$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$O$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$B$O$A$P$F$O$N$O$N$O$N$P$F$O$N$O$N$O$B$O$C$O$A$O$A$O$A$O$N$O$N$O$N$O$D$O$N$O$A$O$A$O$O$O$N$O$N$O$N$O$N$O$N$O$B$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N
-$P$N$P$N$P$N$P$N$P$J$P$J$O$N$O$N$O$N$O$N$O$D$O$N$O$N$O$N$O$N$O$N$O$N$P$E$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$B$O$O$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$B$O$O$O$A$O$F$O$N$O$N$O$N$O$N$O$B$O$C$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N$O$N
-$P$N$P$N$P$H$P$P$P$N$P$N$P$B$P$I$P$I$P$I$P$H$P$H$P$H$P$I$P$I$P$I$P$I$P$F$P$F$P$F$P$F$P$J$P$J$P$J$P$J$P$L$P$F$P$F$P$K$P$I$P$I$P$I$P$B$P$K$P$B$P$B$P$I$P$I$P$I$P$I$P$F$P$L$P$F$P$J$P$J$P$J$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N$O$B$O$F$P$N$P$N$P$N$P$N$O$B$O$F$P$N$P$N$O$F$P$N$P$N$P$N$P$N$P$J$P$J$P$J$P$J$P$L$P$F$P$L$P$I
+"$(O)$N$(O)$N$(O)$N$(O)$N$(O)$B$(O)$N$(O)$C$(O)$N$(O)$A$(O)$N$(O)$A$(O)$N$(O)$A$(O)$N$(O)$A$(O)$C$(O)$N$(O)$N$(O)$A$(O)$A$(O)$N$(O)$N$(O)$C$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$B$(O)$N$(O)$N$(O)$C$(O)$N$(O)$N$(O)$A$(O)$N$(O)$N$(O)$A$(O)$D$(O)$A$(O)$C$(O)$N$(O)$A$(O)$N$(O)$C$(O)$N$(O)$C$(O)$N$(O)$C$(O)$A$(O)$N$(O)$N$(O)$N$(O)$F$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$B$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$F$(O)$N
+$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$B$(O)$N$(O)$C$(O)$N$(O)$A$(O)$N$(O)$C$(O)$A$(O)$N$(O)$N$(O)$A$(O)$A$(O)$N$(O)$N$(O)$A$(O)$C$(O)$N$(O)$N$(O)$C$(O)$N$(O)$N$(O)$N$(O)$N$(O)$B$(O)$N$(O)$N$(O)$A$(O)$N$(O)$N$(O)$A$(O)$N$(O)$C$(O)$A$(O)$N$(O)$C$(O)$N$(O)$A$(O)$N$(O)$C$(O)$N$(O)$A$(O)$C$(O)$N$(O)$N$(O)$N$(O)$A$(O)$N$(O)$N$(O)$F$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$C$(O)$N
+$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$P$F$(O)$(O)$(O)$N$(O)$(O)$(O)$C$(O)$N$(O)$N$(O)$A$(O)$A$(O)$N$(O)$N$(O)$A$(O)$C$(O)$N$(O)$N$(O)$A$(O)$N$(O)$N$(O)$(O)$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$B$(O)$N$(O)$A$(O)$A$(O)$N$(O)$A$(O)$N$(O)$C$(O)$N$(O)$A$(O)$N$(O)$A$(O)$C$(O)$N$(O)$C$(O)$N$(O)$A$(O)$N$(O)$N$(O)$N$(O)$F$(O)$N$(O)$N$(O)$B$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$F$(O)$N$(O)$N$(O)$N$(O)$C$(O)$F$(O)$N
+$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$B$(O)$(O)$(O)$N$(O)$N$(O)$A$(O)$A$(O)$N$(O)$N$(O)$A$(O)$N$(O)$N$(O)$(O)$(O)$N$(O)$A$(O)$N$(O)$N$(O)$N$P$E$(O)$N$(O)$N$(O)$N$(O)$N$(O)$B$(O)$N$(O)$C$(O)$N$(O)$A$(O)$N$(O)$A$(O)$N$(O)$C$(O)$A$(O)$N$(O)$A$P$E$(O)$A$(O)$N$(O)$N$(O)$C$(O)$N$(O)$N$(O)$N$(O)$C$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$C$(O)$A$(O)$N
+$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$P$E$(O)$N$(O)$N$(O)$N$(O)$N$P$E$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$B$(O)$N$(O)$N$(O)$A$(O)$N$(O)$N$(O)$C$(O)$N$(O)$C$(O)$N$(O)$N$(O)$A$(O)$N$(O)$N$(O)$F$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$B$(O)$N$(O)$(O)$(O)$N$(O)$C$(O)$A$(O)$N$(O)$A$(O)$N$(O)$C$(O)$A$(O)$N$(O)$F$(O)$N$(O)$C$(O)$N$(O)$C$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$P$E$(O)$N$(O)$N$(O)$N$(O)$C$(O)$F$(O)$A$(O)$A$(O)$N
+$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$P$F$(O)$B$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$A$(O)$N$(O)$A$(O)$N$(O)$N$(O)$A$(O)$N$(O)$N$(O)$C$(O)$N$(O)$F$(O)$C$(O)$A$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$B$(O)$N$(O)$A$(O)$N$(O)$A$(O)$A$(O)$N$(O)$A$(O)$N$(O)$C$(O)$N$(O)$A$(O)$C$(O)$N$(O)$A$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$A$(O)$N$(O)$A$(O)$A$(O)$N
+$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$(O)$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$P$F$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$D$(O)$N$(O)$B$(O)$N$(O)$N$(O)$A$(O)$N$(O)$N$(O)$C$(O)$N$(O)$N$(O)$F$(O)$C$(O)$N$(O)$F$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$(O)$(O)$A$(O)$N$(O)$A$(O)$N$(O)$C$(O)$N$(O)$C$(O)$A$(O)$N$(O)$C$(O)$N$(O)$N$(O)$N$(O)$N$(O)$F$(O)$N$(O)$N$(O)$N$(O)$A$(O)$C$(O)$A$(O)$N$(O)$A$(O)$A$(O)$N
+$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$P$F$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$P$F$(O)$N$(O)$N$(O)$N$(O)$N$(O)$B$(O)$N$P$E$(O)$A$(O)$N$(O)$N$(O)$A$(O)$A$(O)$N$(O)$C$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$B$(O)$N$(O)$N$(O)$N$(O)$N$(O)$A$(O)$N$(O)$A$(O)$N$(O)$C$(O)$A$(O)$N$(O)$A$(O)$N$(O)$C$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$A$(O)$C$(O)$A$(O)$N$(O)$A$(O)$N$(O)$N
+$(O)$N$(O)$N$(O)$N$P$F$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$P$F$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$P$F$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$A$(O)$A$(O)$N$(O)$A$(O)$F$(O)$C$(O)$N$P$E$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$A$(O)$N$(O)$A$(O)$A$(O)$N$(O)$C$(O)$N$(O)$C$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$C$(O)$A$(O)$A$(O)$A$(O)$N$(O)$N$(O)$N$(O)$N
+$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$P$F$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$P$F$(O)$N$(O)$N$(O)$N$P$F$(O)$N$P$E$(O)$N$(O)$N$(O)$N$(O)$N$P$F$(O)$N$(O)$N$(O)$N$P$F$(O)$N$(O)$N$(O)$B$(O)$N$(O)$A$(O)$A$(O)$A$(O)$F$(O)$N$(O)$N$(O)$D$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$D$(O)$N$(O)$(O)$(O)$A$(O)$N$(O)$A$(O)$A$(O)$A$(O)$N$(O)$N$(O)$F$(O)$N$(O)$N$(O)$A$(O)$A$(O)$B$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N
+$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$P$F$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$P$E$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$P$F$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$D$(O)$C$(O)$B$(O)$A$(O)$M$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$F$(O)$N$(O)$A$(O)$A$(O)$A$(O)$A$(O)$N$(O)$N$(O)$C$(O)$F$(O)$A$(O)$A$(O)$B$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N
+$(O)$N$(O)$N$(O)$N$(O)$D$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$P$F$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$F$(O)$F$(O)$M$(O)$A$P$E$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$F$(O)$N$(O)$A$(O)$N$(O)$C$(O)$B$(O)$A$(O)$A$(O)$D$(O)$N$(O)$A$(O)$A$(O)$A$(O)$B$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N
+$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$P$F$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$D$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$C$(O)$N$(O)$A$(O)$A$(O)$A$(O)$B$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$F$(O)$A$(O)$N$(O)$C$(O)$A$(O)$N$(O)$A$(O)$N$(O)$A$(O)$N$(O)$A$(O)$F$(O)$A$(O)$N$(O)$A$(O)$A$(O)$B$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$P$F$(O)$N$(O)$N
+$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$P$E$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$D$(O)$N$(O)$N$(O)$N$(O)$(O)$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$D$(O)$N$(O)$N$(O)$C$(O)$A$(O)$A$(O)$N$(O)$A$(O)$B$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$C$(O)$F$(O)$D$(O)$F$(O)$N$(O)$A$(O)$A$(O)$N$(O)$A$(O)$A$(O)$N$(O)$A$(O)$N$(O)$A$(O)$C$(O)$B$(O)$A$(O)$A$(O)$N$(O)$B$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N
+$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$P$F$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$P$F$(O)$A$(O)$A$(O)$B$(O)$N$(O)$F$(O)$N$(O)$N$(O)$N$(O)$N$(O)$F$(O)$F$(O)$A$(O)$A$(O)$N$(O)$A$(O)$N$(O)$A$(O)$C$(O)$N$(O)$(O)$(O)$B$(O)$N$(O)$B$(O)$A$(O)$N$(O)$A$(O)$N$(O)$A$(O)$B$(O)$N$(O)$N$(O)$D$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N
+$(O)$N$(O)$N$(O)$N$(O)$N$P$F$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$D$(O)$D$(O)$D$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$A$(O)$A$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$M$(O)$A$(O)$M$(O)$C$(O)$N$(O)$B$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$C$(O)$A$(O)$N$(O)$A$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N
+$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$D$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$P$F$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$P$F$(O)$N$(O)$N$(O)$N$(O)$N$(O)$A$(O)$N$P$F$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$B$(O)$A$(O)$A$(O)$A$(O)$F$(O)$C$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$F$(O)$A$(O)$A$(O)$N$(O)$N$(O)$N$(O)$N$(O)$F$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$F$(O)$N$(O)$N$(O)$N$(O)$N$(O)$B
+$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$(O)$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$B$(O)$A$P$F$(O)$N$(O)$N$(O)$N$P$F$(O)$N$(O)$N$(O)$B$(O)$C$(O)$A$(O)$A$(O)$A$(O)$N$(O)$N$(O)$N$(O)$D$(O)$N$(O)$A$(O)$A$(O)$(O)$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$B$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N
+$P$N$P$N$P$N$P$N$P$J$P$J$(O)$N$(O)$N$(O)$N$(O)$N$(O)$D$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$P$E$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$B$(O)$(O)$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$B$(O)$(O)$(O)$A$(O)$F$(O)$N$(O)$N$(O)$N$(O)$N$(O)$B$(O)$C$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N$(O)$N
+$P$N$P$N$P$H$P$P$P$N$P$N$P$B$P$I$P$I$P$I$P$H$P$H$P$H$P$I$P$I$P$I$P$I$P$F$P$F$P$F$P$F$P$J$P$J$P$J$P$J$P$L$P$F$P$F$P$K$P$I$P$I$P$I$P$B$P$K$P$B$P$B$P$I$P$I$P$I$P$I$P$F$P$L$P$F$P$J$P$J$P$J$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N$(O)$B$(O)$F$P$N$P$N$P$N$P$N$(O)$B$(O)$F$P$N$P$N$(O)$F$P$N$P$N$P$N$P$N$P$J$P$J$P$J$P$J$P$L$P$F$P$L$P$I
 $P$N$P$N$P$N$P$N$P$N$P$N$P$J$P$J$P$J$P$L$P$I$P$I$P$P$P$H$P$H$P$B$P$I$P$I$P$I$P$H$P$P$P$N$P$N$P$N$P$K$P$N$P$N$P$K$P$N$P$K$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$K$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$H$P$H$P$H$P$P$P$I$P$I$P$I$P$L$P$F$P$F$P$J$P$J$P$J$P$N$P$J$P$J$P$L$P$F$P$F$P$I$P$I$P$I$P$H$P$H$P$P$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N
-$P$N$P$N$P$I$P$I$P$H$P$P$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$K$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$B$P$B$P$I$P$I$P$F$P$F$P$L$P$J$P$J$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N"
+$P$N$P$N$P$I$P$I$P$H$P$P$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$K$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$B$P$B$P$I$P$I$P$F$P$F$P$L$P$J$P$J$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N$P$N" > ~/.aurora/next &
 
 
 
